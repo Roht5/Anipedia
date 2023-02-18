41f89b3be688349f28a468e36e3229239fb143d9
Roht5 <rohitbagadeww@gmail.com>
2023-02-18 12:44:00 +0530

    pubs changes

--- anipedia/lib/main.dart
+++ anipedia/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:anipedia/Data%20Store/information.dart';
+import 'package:anipedia/Data Store/information.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'Screens/splash.dart';
