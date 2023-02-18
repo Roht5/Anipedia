86d51d21b85bc6e2e641e08fbef378d89b1450b3
Roht5 <rohitbagadeww@gmail.com>
2023-02-18 13:21:55 +0530

    Stories Information Added

---
+++ anipedia/lib/Data Store/storiesInformation.dart
@@ -0,0 +1,71 @@
+List BookNames=[
+  {
+    'bookName':'Panchatantra',
+    'image':'image',
+    'stories':Panchatantra
+  },
+  {
+    'bookName':'Panchatantra',
+    'image':'image',
+    'stories':Panchatantra
+  }
+];
+List Panchatantra=[
+  {
+    'storyName':'Sparrow And Marbles',
+    'story':[
+      /*
+        pov Types:
+          narrator
+          char1
+          char2
+       */
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'A kind-hearted woodcutter once rescued an eagle from a trap.'
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'Sometime later, the woodcutter had climbed on a steep hill to cuts trees for  wood.'
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'There he got stuck, and couldn\'t come down.'
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'He sat on a rock, waiting for any help to come. '
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'Suddenly the eagle he had rescued came flying down towards him.'
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'In another moment, the eagle flew away with his hat.'
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'The woodcutter became angry and started to chase the eagle'
+      },
+      {
+        'pov':'narrator',
+        'name':'Narrator',
+        'line':'After some time of tracking, he found the way to get down the hill.'
+      },
+      {
+        'pov':'char1',
+        'name':'Wood Cutter',
+        'line':'Oh, so this is why you took my hat. You wanted me to run behind you and find this way. Thank you so much, my friend,'
+      },
+    ],
+    'moral':' Kindness will always get great rewards'
+  }
+];
